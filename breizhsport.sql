ec27642c1507156a7f6102d3d2272c215fb4e961
nicolassssdev <nicolas.lanotte@viacesi.fr>
2024-02-09 14:05:37 +0100

    Ajout colonne image dans produits

--- breizhsport.sql
+++ breizhsport.sql
@@ -1,13 +1,7 @@
--- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
---
--- Host: localhost    Database: breizhsport
--- ------------------------------------------------------
--- Server version	8.0.35-0ubuntu0.22.04.1
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,16 +15,17 @@
 
 DROP TABLE IF EXISTS `Produits`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Produits` (
-  `produit_id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `produit_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `prix` decimal(10,2) DEFAULT NULL,
-  `stock` int DEFAULT NULL,
-  `marque` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `marque` varchar(100) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`produit_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +34,7 @@ CREATE TABLE `Produits` (
 
 LOCK TABLES `Produits` WRITE;
 /*!40000 ALTER TABLE `Produits` DISABLE KEYS */;
+INSERT INTO `Produits` VALUES (1,'Sweat bildoun','Le nouveau sweet de la collection bildoun',12.00,32,'Adidas',NULL);
 /*!40000 ALTER TABLE `Produits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,17 +44,17 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Utilisateurs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Utilisateurs` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hash_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `hash_password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `is_confirmed` tinyint(1) NOT NULL,
-  `picture_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `role` int DEFAULT NULL,
+  `picture_url` varchar(255) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +63,7 @@ CREATE TABLE `Utilisateurs` (
 
 LOCK TABLES `Utilisateurs` WRITE;
 /*!40000 ALTER TABLE `Utilisateurs` DISABLE KEYS */;
+INSERT INTO `Utilisateurs` VALUES (1,'Electrix67130','$2b$10$G4.knwPT5FexqSTKhSW/9.9VJm0ScUfwig2FMSRKH6U/5KCA3Rqc.','electrix@gmail.com',0,NULL,NULL),(2,'Louis','$2b$10$923gzNztTpKEoudhAW9K4.86gn7ko6mvca8St5MEcFNbAY3op73jW','louis@gmail.com',0,NULL,NULL),(3,'Maxime','$2b$10$6Ob6lliicpES/hbd4wJoYuxR1CJVmcJGh8eMeIS4w9fDfKWY05g7y','maxime@gmail.com',0,NULL,NULL),(4,'Nicolas','$2b$10$CUoHzITnqbMRwP4exeM3xOh63PQ6LWIpIqO5AvpryFuvZJlt/0riG','nicolas@gmail.com',0,NULL,NULL),(5,'Julien','$2b$10$qJyxCeG3wULTucIaiCwXY.mfDih1pRNrPIKkYxjPRT0boIfEcEPSC','julien@gmail.com',0,NULL,NULL);
 /*!40000 ALTER TABLE `Utilisateurs` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -79,4 +76,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-05  9:41:42
